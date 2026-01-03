abca81b2b086b81379f2f51feedbba059f6b1035
Jannatul Eusra <eusra2207076@stud.kuet.ac.bd>
2026-01-04 02:48:11 +0600

    Added Welcome Page

---
+++ src/main/java/org/example/WelcomeController.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class WelcomeController {
+
+    @FXML
+    private void onRegister(ActionEvent event) {
+        System.out.println("Sign Up clicked");
+    }
+}
